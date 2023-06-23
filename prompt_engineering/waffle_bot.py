d32f2ec3fea926ef26d726c1bade1b5f2da68a58
codeananda <51246969+codeananda@users.noreply.github.com>
2023-06-23 14:06:24 +0100

    Greatly simplified code

--- prompt_engineering/waffle_bot.py
+++ prompt_engineering/waffle_bot.py
@@ -7,116 +7,103 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 from streamlit_chat import message
 
-# Setting page title and header
-st.set_page_config(page_title="Waffle House Order Bot", page_icon=":waffle:")
-st.title("Waffle House Order Bot 🧇")
-intro = """Welcome to the Waffle House, the place where all your waffle filled dreams come true.
-
-Start chatting with WaffleBot below to find out what you can order, how much it costs, and how to pay."""
-st.markdown(intro)
-attribution = f"Created by [Adam Murphy](https://github.com/codeananda)"
-attribution += f" - [Source Code](https://github.com/codeananda/ChatGPT_Projects/blob/main/prompt_engineering/WaffleHouse.py)"
-st.markdown(attribution)
-st.markdown("---")
+MODEL = "gpt-3.5-turbo"
+AVATAR = "thumbs"
 
 # Set org ID and API key
 _ = load_dotenv(find_dotenv())
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORG_ID")
 
-# Initialise session state variables
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-if "past" not in st.session_state:
-    st.session_state["past"] = []
 
-waffle_prompt = """You are the Waffle House order bot. You are a helpful assistant and will help \
-the customer order their meal. Be friendly and kind at all times. \
-First greet the customer, then collect the order and then ask if it's pick up or delivery. \
-You wait to collect the entire order, then summarize it and check for a final time if the \
-customer wants to add anything else. \
-Always summarize the entire order before collecting payment. \
-If it's a delivery, ask them for their address. \
-If it's pick up, tell them our address: 123 Waffle House Lane, London. \
-Finally collect the payment. Ask if they want to pay by credit card or cash. \
-If they say credit card say 'Please click the link below to pay by credit card'. \
-If they say cash, say they can pay when they pick up the order or pay the delivery driver. \
-Make sure to clarify all options, extras and sizes to uniquely identify the order. \
-The menu is: \
-Waffle type: normal ($10), gluten-free ($10), protein ($1 extra) \
-Toppings: strawberries, blueberries, chocolate chips, whipped cream, butter, syrup, bacon \
-Each topping costs $1 \
-Drinks: coffee, orange juice, milk, water \
-Each drink costs $2 \
-Once the order is complete, output the order summary and total cost in JSON format. \
-Itemize the price for each item. The fields should be 1) waffle_type, 2) list of toppings \
-3) list of drinks, 4) total price (float)
-"""
-
-initial_state = [
-    {"role": "system", "content": waffle_prompt},
-    {
-        "role": "assistant",
-        "content": "Hello, welcome to Waffle House! What can I get for you today?",
-    },
-]
-if "messages" not in st.session_state:
-    st.session_state["messages"] = initial_state
-
-# Sidebar - let user clear the current conversation
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
-model = "gpt-3.5-turbo"
-
-
-# reset everything
-if clear_button:
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["messages"] = initial_state
-
-
-# generate a response
 def generate_response(prompt):
+    """Send prompt to OpenAI and return the response. Add the prompt and response to the session state."""
     st.session_state["messages"].append({"role": "user", "content": prompt})
 
     completion = openai.ChatCompletion.create(
-        model=model, messages=st.session_state["messages"]
+        model=MODEL, messages=st.session_state["messages"]
     )
     response = completion.choices[0].message.content
     st.session_state["messages"].append({"role": "assistant", "content": response})
     return response
 
 
-wh_avatar = "thumbs"
+def get_system_prompt():
+    """Define system prompt for the chatbot."""
+    system_prompt = """You are the Waffle House order bot. You are a helpful assistant and will help 
+    the customer order their meal. Be friendly and kind at all times. 
+    First greet the customer, then collect the order and then ask if it's pick up or delivery. \
+    You wait to collect the entire order, then summarize it and check for a final time if the \
+    customer wants to add anything else. \
+    Always summarize the entire order before collecting payment. \
+    If it's a delivery, ask them for their address. \
+    If it's pick up, tell them our address: 123 Waffle House Lane, London. \
+    Finally collect the payment. Ask if they want to pay by credit card or cash. \
+    If they say credit card say 'Please click the link below to pay by credit card'. \
+    If they say cash, say they can pay when they pick up the order or pay the delivery driver. \
+    Make sure to clarify all options, extras and sizes to uniquely identify the order. \
+    The menu is: \
+    Waffle type: normal ($10), gluten-free ($10), protein ($1 extra) \
+    Toppings: strawberries, blueberries, chocolate chips, whipped cream, butter, syrup, bacon \
+    Each topping costs $1 \
+    Drinks: coffee, orange juice, milk, water \
+    Each drink costs $2 \
+    Once the order is complete, output the order summary and total cost in JSON format. \
+    Itemize the price for each item. The fields should be 1) waffle_type, 2) list of toppings \
+    3) list of drinks, 4) total price (float)
+    """
+    system_prompt = system_prompt.replace("\n", " ")
+    return system_prompt
 
-message(
-    "Welcome to the Waffle House! What can I get for you?",
-    avatar_style=wh_avatar,
-)
 
-# container for chat history
+# Top matter
+st.set_page_config(page_title="Waffle House Order Bot", page_icon=":waffle:")
+st.title("Waffle House Order Bot 🧇")
+
+intro = """Welcome to the Waffle House, the place where all your waffle filled dreams come true.
+
+Start chatting with WaffleBot below to find out what you can order, how much it costs, and how to pay."""
+st.markdown(intro)
+
+attribution = f"""Created by [Adam Murphy](https://github.com/codeananda) - \
+[Source Code](https://github.com/codeananda/ChatGPT_Projects/blob/main/prompt_engineering/waffle_bot.py)
+
+Need a chatbot built? Reach out via my [Upwork profile](https://www.upwork.com/freelancers/~01153ca9fd0099730e) \
+to schedule a call and see how I can help."""
+st.markdown(attribution)
+
+st.markdown("---")
+
+initial_state = [
+    {"role": "system", "content": get_system_prompt()},
+    {'role': 'assistant', 'content': '👋 Welcome to Waffle House! What can I get for you?'}
+]
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = initial_state
+
+# Let user clear the current conversation
+clear_button = st.sidebar.button("Clear Conversation", key="clear")
+if clear_button:
+    st.session_state["messages"] = initial_state
+
+# Chat history container
 response_container = st.container()
-# container for text box
+# Text input container
 input_container = st.container()
-
 
 with input_container:
     with st.form(key="my_form", clear_on_submit=True):
-        user_input = st.text_area("You:", key="input", height=100)
+        user_input = st.text_area("You:", height=100)
         submit_button = st.form_submit_button(label="Send")
 
     if submit_button and user_input:
         output = generate_response(user_input)
-        st.session_state["past"].append(user_input)
-        st.session_state["generated"].append(output)
 
-
-if st.session_state["generated"]:
+if st.session_state["messages"]:
     with response_container:
-        for i in range(len(st.session_state["generated"])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
-            message(
-                st.session_state["generated"][i],
-                key=str(i),
-                avatar_style=wh_avatar,
-            )
+        for message_ in st.session_state.messages:
+            if message_["role"] == "user":
+                message(message_["content"], is_user=True)
+            elif message_['role'] == 'assistant':
+                message(message_["content"], avatar_style=AVATAR)
