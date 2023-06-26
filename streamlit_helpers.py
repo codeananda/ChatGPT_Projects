8ff573ed1fec7fe9e95abef51d2c11c6f86cc782
codeananda <51246969+codeananda@users.noreply.github.com>
2023-06-26 18:10:00 +0100

    Added temperature to generate_reponse

--- streamlit_helpers.py
+++ streamlit_helpers.py
@@ -1,12 +1,15 @@
 import openai
 import streamlit as st
 
-def generate_response(prompt):
-    """Send prompt to OpenAI and return the response. Add the prompt and response to the session state."""
+def generate_response(prompt, temperature=0):
+    """Send prompt to OpenAI and return the response. Add the prompt and response to
+    the session state."""
     st.session_state["messages"].append({"role": "user", "content": prompt})
 
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=st.session_state["messages"]
+        model="gpt-3.5-turbo",
+        messages=st.session_state["messages"],
+        temperature=temperature
     )
     response = completion.choices[0].message.content
     st.session_state["messages"].append({"role": "assistant", "content": response})
